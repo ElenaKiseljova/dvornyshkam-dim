89c6ae701c4357984c4f6e171310dcbeba6a27c4
Elena Kiseljova <e.a.kiseljova@gmail.com>
2024-11-18 22:05:11 +0200

    upd: get test data from back-end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-
 import { Suspense } from 'react';
+
+import { useAxios } from './hooks/useAxios';
+
+import { getAnimals } from './api/index.ts';
 
 import Header from './components/header/Header';
 import HeroSection from './components/heroSection/HeroSection';
@@ -10,21 +13,23 @@ import HelpSection from './components/helpSection/HelpSection';
 import DonateSection from './components/donateSection/DonateSection';
 import Footer from './components/footer/Footer';
 
-import './index.scss'
-
-
+import './index.scss';
 
 function App() {
+  const { data, error, loaded } = useAxios(getAnimals(), 'GET');
+
+  console.log(data, error, loaded);
+
   return (
     <div className="App">
-        <Header />
-        <HeroSection />
-        <SearchSection />
-        <AboutSection />
-        <OurAnimals />
-        <HelpSection />
-        <DonateSection />
-        <Footer />
+      <Header />
+      <HeroSection />
+      <SearchSection />
+      <AboutSection />
+      <OurAnimals />
+      <HelpSection />
+      <DonateSection />
+      <Footer />
     </div>
   );
 }
@@ -34,5 +39,5 @@ export default function wrapperedApp() {
     <Suspense fallback="...loading">
       <App />
     </Suspense>
-  )
+  );
 }
